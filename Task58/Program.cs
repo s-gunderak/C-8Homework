f1dc0a00cbc8b705f82e7340db05d521151fa9ac
gunderak <sergey.gund@gmail.com>
2023-03-12 22:13:43 +0300

    add Task58

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -5,3 +5,64 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+Console.Write("Введите количество строк матрицы A: ");
+int am = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы A: ");
+int an = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество строк матрицы B: ");
+int bm = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы B: ");
+int bn = int.Parse(Console.ReadLine());
+
+int[,] matrixA = GetArray(am, an, 0, 9);
+PrinMtatrix(matrixA);
+Console.WriteLine("----");
+int[,] matrixB = GetArray(bm, bn, 0, 9);
+PrinMtatrix(matrixB);
+
+Console.WriteLine("Матрица C = матрица A * матрица B:");
+int[,] matrixC = Multiplication(matrixA, matrixB);
+PrinMtatrix(matrixC);
+
+int[,] GetArray(int m, int n, int minVal, int maxVal)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minVal, maxVal);
+        }
+    }
+    return result;
+}
+
+void PrinMtatrix(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Multiplication(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+    int[,] result = new int[a.GetLength(0), b.GetLength(1)];
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                result[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    return result;
+}
